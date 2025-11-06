cd99d07ad3f05e75df4fe7ccd6243dcd2853dfa4
Aqsa Ahmed <filinta9057@gmail.com>
2025-11-06 22:50:43 +0500

    Delete FoodApp directory

--- FoodApp/frontend/test/HomeScreen.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react-native";
-import HomeScreen from "../screens/HomeScreen";
-
-test("HomeScreen renders properly", () => {
-  const screen = render(<HomeScreen />);
-  expect(screen).toBeTruthy();
-});
